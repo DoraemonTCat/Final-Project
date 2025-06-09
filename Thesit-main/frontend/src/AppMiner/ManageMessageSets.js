e29321bff64ead15db702e7d4afda64fe81a8f8c
DoraemonTCat <peeframesave@gmail.com>
2025-06-09 13:22:52 +0700

    pp

--- Thesit-main/frontend/src/AppMiner/ManageMessageSets.js
+++ Thesit-main/frontend/src/AppMiner/ManageMessageSets.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { fetchPages, getMessageSetsByPage, connectFacebook, updateMessageSet, deleteMessageSet } from '../Features/Tool';
-import '../CSS/Default.css';
+import '../CSS/ManageMessageSets.css';
 
 function ManageMessageSets() {
     const [pages, setPages] = useState([]);
