62dc316f79c42dda3d691a2083d3d8835572696f
DoraemonTCat <peeframesave@gmail.com>
2025-08-17 22:02:53 +0700

    แก้ได้ละ555

--- backend/app/task/scheduler.py
+++ backend/app/task/scheduler.py
@@ -82,10 +82,10 @@ def start_scheduler():
     scheduler = BackgroundScheduler()
     
     # Sync ข้อมูลลูกค้าทุกนาที
-    scheduler.add_job(schedule_facebook_sync, 'interval', minutes=10)
+    scheduler.add_job(schedule_facebook_sync, 'interval', minutes=1)
     
     # Sync ข้อความทุกชั่วโมง
-    scheduler.add_job(schedule_facebook_messages_sync, 'interval', hours=1)
+    scheduler.add_job(schedule_facebook_messages_sync, 'interval', minutes=1)
 
     # clssi
     scheduler.add_job(scheduled_hybrid_classification, 'interval', minutes=1)
